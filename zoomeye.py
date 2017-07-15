9ece4e90315bebfbf594e61f46b1dcff03f428cb
Alexander Cardosi <alexanderdcardosi@gmail.com>
2017-07-14 20:02:22 -0400

    Made everything functions

--- zoomeye.py
+++ zoomeye.py
@@ -9,8 +9,6 @@ USER_EMAIL = "email@example.com"
 USER_PASSWORD = "password"
 ##############################################################################
 
-
-
 parser = argparse.ArgumentParser(description='Simple ZoomEye searcher, outputs IPs to stdout or file')
 
 parser.add_argument("search", help="Your ZoomEye Search")
@@ -35,64 +33,70 @@ USER_DATA='{"username": ' + '"'+EMAIL+'"' + ', "password":  ' + '"'+PASSWORD+'"'
 AUTH_REQUEST = requests.post(API_URL + '/user/login', data=USER_DATA)
 
 # Attempts to access the raw token, if there is a KeyError it means the credentials are incorrect
-try:
-  ACCESS_TOKEN = AUTH_REQUEST.json()['access_token'];
-except KeyError:
-  print("[ERROR] Invalid Credentials, please specify an email and password either in this file or with `--email` and `--password` arguments")
-  quit()
-  
-# This is the prefixed Token
-TOKEN = "JWT " + ACCESS_TOKEN
-
-# Add the prefixed token to the headers
-HEADERS = {"Authorization": TOKEN}
-
-
-if args.save:
-  print("You have enabled save. All IPs will be saved to results.txt.")
+def getToken():
   try:
-    resultsFile = open('results.txt', 'w')
-  except:
-    print("[ERROR] Could not write to results.txt, please check your permissions")
+    ACCESS_TOKEN = AUTH_REQUEST.json()['access_token'];
+    return ACCESS_TOKEN
+  except KeyError:
+    print("[ERROR] Invalid Credentials, please specify an email and password either in this file or with `--email` and `--password` arguments")
     quit()
+
+
+def detectSaveMode():
+  if args.save:
+    print("You have enabled save. All IPs will be saved to results.txt")
+    try:
+      global resultsFile
+      resultsFile = open('results.txt', 'w')
+    except:
+      print("[ERROR] Could not write to results.txt, please check your permissions")
+      quit()
     
 # Loopy loop
-currentPage = 1
-while currentPage <= PAGECOUNT:
-  if args.save:
-    print("\nPage " + str(currentPage))
+def getResult():
+  # This is the prefixed Token
+  TOKEN = "JWT " + getToken()
   
-  SEARCH = requests.get(API_URL + '/' + SEARCH_TYPE + '/search', headers=HEADERS, params={"query": QUERY, "page": currentPage})
-  response = json.loads(SEARCH.text)
-  i = 0
-  try:
-    while i < len(response["matches"]):
-      if SEARCH_TYPE == "host":
-        print(response["matches"][i]["ip"])
-        if args.save:
-          resultsFile.write(response["matches"][i]["ip"] + "\n")
-      if SEARCH_TYPE == "web":
-        print(response["matches"][i]["ip"][0])
-        if args.save:
-          resultsFile.write(response["matches"][i]["ip"][0] + "\n")
-      
-
-      
-      i += 1
-  except IndexError:
-    break
-  except KeyError:
-    print("[ERROR] No hosts found")
-    quit()
-  currentPage += 1
-
-
-
-
-if args.save:
-  resultsFile.close() # Close as write
-  ip_count = sum(1 for line in open("results.txt", "r")) # Open as read
-  print("\n" + str(ip_count) + " IPs saved to results.txt.")
+  # Add the prefixed token to the headers
+  HEADERS = {"Authorization": TOKEN}
+  
+  currentPage = 1
+  while currentPage <= PAGECOUNT:
+    if args.save:
+      print("\nPage " + str(currentPage))
+    
+    SEARCH = requests.get(API_URL + '/' + SEARCH_TYPE + '/search', headers=HEADERS, params={"query": QUERY, "page": currentPage})
+    response = json.loads(SEARCH.text)
+    i = 0
+    try:
+      while i < len(response["matches"]):
+        if SEARCH_TYPE == "host":
+          print(response["matches"][i]["ip"])
+          if args.save:
+            resultsFile.write(response["matches"][i]["ip"] + "\n")
+        if SEARCH_TYPE == "web":
+          print(response["matches"][i]["ip"][0])
+          if args.save:
+            resultsFile.write(response["matches"][i]["ip"][0] + "\n")
+        i += 1
+    except IndexError:
+      break
+    except KeyError:
+      print("[ERROR] No hosts found")
+      ipCount()
+      quit()
+    currentPage += 1
 
 
+def ipCount():
+  if args.save:
+    resultsFile.close() # Close as write
+    ip_count = sum(1 for line in open("results.txt", "r")) # Open as read
+    print("\n" + str(ip_count) + " IPs saved to results.txt")
+    
+    
 
+
+detectSaveMode()
+getResult()
+ipCount()
